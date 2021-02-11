642c398929ba12c8c39da86d97d1b43f7ae85744
mavillegas04 <mavillegas2015@gmail.com>
2021-02-11 07:41:21 -0500

    clase 2

---
+++ clases/clase2.py
@@ -0,0 +1,23 @@
+pruebaT = True
+pruebaF = False
+print (pruebaT)
+print (pruebaF) 
+edad = 18
+estatura = 1.66
+peso = 56 
+nombre = "Mariana Villegas"
+print ("#"*15, "mayor edad", "#"*15)
+ismayoredad = edad <= 18 
+print (ismayoredad)
+print ("#"*15, "bajo la estatura promedio", "#"*15)
+ismayorestatura = estatura < 1.70 
+print (ismayorestatura) 
+#calculando si el peso es diferente a 56
+print ("#"*15, "peso diferente 56", "#"*15)
+ispesodiferente = peso != 56
+print (ispesodiferente)
+# vamos a ver si un apellido esta en el nombre completo 
+apellido = "Villegas"
+isapellido = apellido in nombre 
+print ("#"*15, "esta apellido en el nombre", "#"*15)
+print (isapellido)
