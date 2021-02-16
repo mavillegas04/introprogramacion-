b5a62c63690c770043bd4b3604a1a94758aaf3eb
mavillegas04 <mavillegas2015@gmail.com>
2021-02-15 21:27:27 -0500

    poniedno procedimientos

--- clases/operaciones.py
+++ clases/operaciones.py
@@ -1,13 +1,18 @@
 numeroA = 87 
 numeroB = 83 
+# estamos sumando A y B
 sumar = numeroA + numeroB
 print ("el resultado de la suma es", sumar)
 print (f"la suma dio {sumar} exitosamente")
+# estamos restando A y B
 restar = numeroA - numeroB
 print (f"la restar dio {restar} exitosamente")
+# estamos multiplicando A y B
 multiplicar = numeroA * numeroB
 print (f"la multiplicar dio {multiplicar} exitosamente")
+# estamos diviendo A y B
 dividir = numeroA / numeroB
 print (f"la dividir dio {dividir} exitosamente")
+# estamos sacando el exponente de A y B
 exponente = numeroA**numeroB
 print (f"la exponente dio {exponente} exitosamente")
