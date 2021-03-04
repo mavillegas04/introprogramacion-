57d9ab8e352169aa112485b28dd45bb2db2e71a3
mavillegas04 <mavillegas2015@gmail.com>
2021-03-04 07:21:31 -0500

    calse

--- clases/juego2.py
+++ clases/juego2.py
@@ -28,5 +28,4 @@ while (numeroingresado != numerooculto and vidas >1):
 if (vidas >=0 and numeroingresado == numerooculto):
     print (mensajeganaste)
 else :
-    print (mensajeperdiste "El numero era el", numerooculto)
-    
+    print (mensajeperdiste "el numero era el", numerooculto)
