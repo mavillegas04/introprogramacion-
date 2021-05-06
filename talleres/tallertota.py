e5889ddd31155a5598b019c26e72150f12f6e887
mavillegas04 <mavillegas2015@gmail.com>
2021-05-06 08:12:15 -0500

    taller

--- talleres/tallertota.py
+++ talleres/tallertota.py
@@ -1,8 +1,8 @@
 # graficos de torta 
 import matplotlib.pyplot as plt
 ciudades = ['medellin', 'cali', 'cartagena', 'bogota', 'barranquilla']
-habitantes = [2.569, 2.228, 914.552, 7.181, 1.206]
-pieExplode = [0,1,0,0,0,0]
+habitantes = [2.569, 2.228, 1.050, 7.181, 1.206]
+pieExplode = [0,1,0,0,0]
 
 acumulado = 0 
 
@@ -14,6 +14,3 @@ plt.pie (habitantes, explode = pieExplode, labels = ciudades, shadow = True)
 plt.savefig ('tallertota.png')
 plt.show () 
 
-# profe no me esta queriendo generar el grafico no se porque 
-# no me aparece ningun error ni nada solo no me lo genera
-# no se si depronto me falta algo por descargar  
